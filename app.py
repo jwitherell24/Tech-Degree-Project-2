f26b8a08250c09f77da9c80fd2d5e539509da5a5
Josh Witherell <witherellj24@gmail.com>
2022-08-04 21:52:33 -0400

    First upload
    I meant to do this after reading instructions for project, but completely forgot until now! This first upload contains preliminary functions for cleaned_data and balance_teams

---
+++ app.py
@@ -0,0 +1,44 @@
+from constants import TEAMS
+from constants import PLAYERS
+
+
+def cleaned_data(PLAYERS):
+    cleaned_players = []
+    for player in PLAYERS:
+        fixed = {}
+        fixed["name"] = player["name"]
+        fixed["guardians"] = player["guardians"]
+        if player["experience"] == "YES":
+            fixed["experience"] = True
+        else:
+            fixed["experience"] = False
+        fixed["height"] = int(player["height"].split(" ")[0])
+        cleaned_players.append(fixed)
+
+    return cleaned_players
+
+def balance_teams(PLAYERS, TEAMS):
+    cleaned_data(PLAYERS)
+    while True:
+        Panthers = []
+        Bandits = []
+        Warriors = []
+        for player in cleaned_data(PLAYERS):
+            if len(Panthers) < 6:
+                Panthers.append(player)
+                continue
+            if len(Bandits) < 6:
+                Bandits.append(player)
+                continue
+            if len(Warriors) < 6:
+                Warriors.append(player)
+                continue
+            else:
+                break
+
+        return Panthers, Bandits, Warriors
+
+print(cleaned_data(PLAYERS))
+print(balance_teams(PLAYERS, TEAMS))
+
+#if __name__ == "__main__":
