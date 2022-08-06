7a3634a99dbd54cbcf9ea7e9286ace68016a48f6
Josh Witherell <witherellj24@gmail.com>
2022-08-05 20:44:35 -0400

    Placed dunder main where I believe it'll need to go

--- app.py
+++ app.py
@@ -1,46 +1,41 @@
 from constants import TEAMS
 from constants import PLAYERS
 
-
-def cleaned_data(PLAYERS):
-    cleaned_players = []
-    for player in PLAYERS:
-        fixed = {}
-        fixed["name"] = player["name"]
-        fixed["guardians"] = player["guardians"]
-        if player["experience"] == "YES":
-            fixed["experience"] = True
-        else:
-            fixed["experience"] = False
-        fixed["height"] = int(player["height"].split(" ")[0])
-        cleaned_players.append(fixed)
-        for key, value in fixed.items():
-            print("{}: {}".format(key, value))
-
-    return cleaned_players
-
-def balance_teams(PLAYERS, TEAMS):
-    cleaned_data(PLAYERS)
-    while True:
-        Panthers = []
-        Bandits = []
-        Warriors = []
-        for player in cleaned_data(PLAYERS):
-            if len(Panthers) < 6:
-                Panthers.append(player)
-                continue
-            if len(Bandits) < 6:
-                Bandits.append(player)
-                continue
-            if len(Warriors) < 6:
-                Warriors.append(player)
-                continue
+if __name__ == "__main__":
+    def cleaned_data(PLAYERS):
+        cleaned_players = []
+        for player in PLAYERS:
+            fixed = {}
+            fixed["name"] = player["name"]
+            fixed["guardians"] = player["guardians"]
+            if player["experience"] == "YES":
+                fixed["experience"] = True
             else:
-                break
+                fixed["experience"] = False
+            fixed["height"] = int(player["height"].split(" ")[0])
+            cleaned_players.append(fixed)
+            for key, value in fixed.items():
+                print("{}: {}".format(key, value))
 
-        return Panthers, Bandits, Warriors
+        return cleaned_players
 
-cleaned_data(PLAYERS)
-#print(balance_teams(PLAYERS, TEAMS))
+    def balance_teams(PLAYERS, TEAMS):
+        cleaned_data(PLAYERS)
+        while True:
+            Panthers = []
+            Bandits = []
+            Warriors = []
+            for player in cleaned_data(PLAYERS):
+                if len(Panthers) < 6:
+                    Panthers.append(player)
+                    continue
+                if len(Bandits) < 6:
+                    Bandits.append(player)
+                    continue
+                if len(Warriors) < 6:
+                    Warriors.append(player)
+                    continue
+                else:
+                    break
 
-#if __name__ == "__main__":
+            return Panthers, Bandits, Warriors
