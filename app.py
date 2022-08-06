6dbf622dd12960f910da9bfb52190698ecc52b02
Josh Witherell <witherellj24@gmail.com>
2022-08-05 22:30:55 -0400

    Updated to balance teams function, saved at point I need to experiment to figure out rest of function

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from constants import TEAMS
 from constants import PLAYERS
 import copy
+import random
+
 
 teams = TEAMS.copy()
 players = PLAYERS.copy()
 cleaned_players = []
+num_players_team = len(players) / len(teams)
+panthers = []
+bandits = []
+warriors = []
+experienced = []
+inexperienced = []
 
 def clean_data(players):
     for player in players:
@@ -19,11 +27,23 @@ def clean_data(players):
         cleaned_players.append(fixed)
         for key, value in fixed.items():
             if key == "name":
-                print("\n{}:{}".format(key, value))
+                print("\n{}: {}".format(key, value))
             else:
-                print("{}:{}".format(key, value))
+                print("{}: {}".format(key, value))
+
+def balance_teams(cleaned_players):
+    equal_experience = num_players_team / 2
+    for player in cleaned_players:
+        if player["experience"] == True:
+            experienced.append(player)
+        else:
+            inexperienced.append(player
+
+
+
+
 
 def main():
     clean_data(players)
-
+    balance_teams(cleaned_players)
 #if __name__ == "__main__":
